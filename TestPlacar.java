d237d5b3649d8b485576f4cc0fc8e4bd2f6917d1
Aridio Silva <aridiosilva@aridiosilva.com>
2020-12-05 11:56:11 -0300

    Add files via upload

--- TestPlacar.java
+++ TestPlacar.java
@@ -3,6 +3,8 @@ package courseraita;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+
 import org.junit.jupiter.api.Test;
 
 class TestPlacar {
@@ -11,10 +13,13 @@ class TestPlacar {
 	Placar _placar = new Placar (_mock);
 	
 	@Test
-	void testIncuiPontuacaoPrimeiroUsuario() {
-
+	void testIncuiPontuacaoPrimeiroUsuario() throws Exception {
 		Usuarios usuario1 = new Usuarios("guerra", "estrela", 10);
-		assertEquals (true,_placar.registrarPontosDoUsuario (usuario1));
+		_placar.registrarPontosDoUsuario (usuario1);
+		assertEquals (10,_mock.recuperarQuantoPontosDeUmTipoTemOUsuario("estrela", "guerra"));
+		HashSet<String> _tiposDePontos =  _mock.retornarTiposDePontosRegistrados();
+		assertEquals (1,_tiposDePontos.size());
+		assertEquals (true,_tiposDePontos.contains("estrela"));
 	}
 
 }
