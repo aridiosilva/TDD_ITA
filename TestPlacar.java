45cf38ca8409087bd6eaca28eef0cea0c0a4f546
Aridio Silva <aridiosilva@aridiosilva.com>
2020-12-05 17:38:05 -0300

    Add files via upload

--- TestPlacar.java
+++ TestPlacar.java
@@ -6,20 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.HashSet;
 
 import org.junit.jupiter.api.Test;
+import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder (MethodSorters.NAME_ASCENDING)
 class TestPlacar {
 
 	MockArmazenamento _mock = new MockArmazenamento();
 	Placar _placar = new Placar (_mock);
 
 	@Test
-	void testIncuiPontuacaoPrimeiroUsuario() throws Exception {
-		_placar.registrarPontosDoUsuario (new Usuarios("guerra", "estrela", 10));
-		assertEquals (10,_mock.recuperarQuantoPontosDeUmTipoTemOUsuario("estrela", "guerra"));
-		HashSet<String> _tiposDePontos =  _mock.retornarTiposDePontosRegistrados();
+	void test001() throws Exception {
+		
+		_placar.registrarPontosDoUsuario(new Usuarios("francisco", "estrela", 19));
+		assertEquals (19,_mock.recuperarQuantoPontosDeUmTipoTemOUsuario("estrela", "francisco"));
+		HashSet<String> _tiposDePontos = _mock.retornarTodosOsTiposDePontosJaRegistradosparaAlgumUsuario();
 		assertEquals (1,_tiposDePontos.size());
 		assertEquals (true,_tiposDePontos.contains("estrela"));
 	}
-
-	
 }
