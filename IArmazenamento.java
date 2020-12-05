00f1ddea0266c1b895b9201cc2c16d29657195f0
Aridio Silva <aridiosilva@aridiosilva.com>
2020-12-05 16:32:12 -0300

    Add files via upload

--- IArmazenamento.java
+++ IArmazenamento.java
@@ -1,13 +1,14 @@
 
 package courseraita;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 public interface IArmazenamento {
 
 	public void armazenarQtePontosDeUmTipoRecebidoPeloUsuario (Usuarios u);
 	public HashSet<String> retornarTiposDePontosRegistrados ();
-	public List<Usuarios> retornarUsuariosComPontosDiferenteZero ();
+	public LinkedList<Usuarios> retornarTodosUsuariosComAlgumTipodePonto () throws Exception;
 	public int recuperarQuantoPontosDeUmTipoTemOUsuario (String tipoPonto, String usuario) throws Exception;
 
 }
--- MockArmazenamento.java
+++ MockArmazenamento.java
@@ -1,12 +1,8 @@
 package courseraita;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.TreeSet;
 
 public class MockArmazenamento implements IArmazenamento {
 
@@ -14,14 +10,10 @@ public class MockArmazenamento implements IArmazenamento {
 	private static HashSet<String>        _diferentesTiposDePontos = new HashSet<String>();
 	private static HashSet<String>        _usuariosCadastrados     = new HashSet<String>();
 	private static HashMap<String,String> _usuariosETiposDePontos  = new HashMap<String,String>();
-
-	private void exibe (String msg) {
-		System.out.println(msg);
-	}
+	final int SEM_PONTUACAO = 0;
 
 	@Override
 	public void armazenarQtePontosDeUmTipoRecebidoPeloUsuario(Usuarios u) {
-
 		_pontuacaoUsuarios.add (u);
 		_diferentesTiposDePontos.add (u._tipoPonto);
 		_usuariosCadastrados.add(u._usuario);
@@ -30,46 +22,68 @@ public class MockArmazenamento implements IArmazenamento {
 		exibe (	"\n Registro Adicionado -> " + u.getUsuario()  + ", " + u.getTipoPonto() +  ", " + u.getPontos());
 		exibe ("tipos de pontos = " + _diferentesTiposDePontos);
  		System.out.println("Lista de Usuarios= " +  _usuariosCadastrados );
-
 	}
 
 	@Override
 	public HashSet<String> retornarTiposDePontosRegistrados() {
-
 		System.out.println( _diferentesTiposDePontos + "\n");
 		return _diferentesTiposDePontos;
 	}
 
 	@Override
-	public List<Usuarios> retornarUsuariosComPontosDiferenteZero() {
-
-		return null;
+	public LinkedList<Usuarios> retornarTodosUsuariosComAlgumTipodePonto() throws Exception {
+		if (_pontuacaoUsuarios.isEmpty())
+			throw new Exception ("Arquivo Vazio - Nenhuma Pontuacao de Usuario");
+		LinkedList<Usuarios> _usuariosEncontrados = _pontuacaoUsuarios;
+		boolean acheiUser = false;
+		for (int i = 0; i < _usuariosEncontrados.size(); i++) {
+		    String name       = (String) _usuariosEncontrados.get(i).getUsuario();
+		    String typePoints = (String) _usuariosEncontrados.get(i).getTipoPonto();
+		    int points        = (int)    _usuariosEncontrados.get(i).getPontos();
+		    System.out.println(" REG=" + i + " -->)" + name + " , " + typePoints + " , " + points );
+			if (points == SEM_PONTUACAO || points < SEM_PONTUACAO) {
+				_usuariosEncontrados.remove(i);	
+		        System.out.println (" Reg.Pontuaçao Zero ou Negativo Removidos da Lista-Usuarios = " + name + " , " + typePoints );
+			}
+			else acheiUser = true;
+		}
+		if (!acheiUser) {
+			throw new Exception ("Arquivo Não Vazio - Porém Nenhum Usuario com Pontuacao maior do que Zero");
+		}
+		System.out.println(_usuariosEncontrados);
+		return _usuariosEncontrados;
 	}
 	
 	@Override
 	public int recuperarQuantoPontosDeUmTipoTemOUsuario(String tipoPonto, String usuario) throws Exception {
-
-		if (_pontuacaoUsuarios.size() == 0)
+		if (_pontuacaoUsuarios.isEmpty())
 			throw new Exception ("Arquivo Vazio - Nenhuma Pontuacao de Usuario");
-
 		exibe("Procurar por -> " + usuario  + " , " + tipoPonto);
-
 		int _totalPontos = 0;
+		boolean acheiUser = false;	
 		for (int i = 0; i < _pontuacaoUsuarios.size(); i++) {
-			
-		    System.out.println(" REG=" + i + " -->)" + _pontuacaoUsuarios.get(i).getUsuario() + " , " + 
-		    		_pontuacaoUsuarios.get(i).getTipoPonto() + " , " +
-		    		_pontuacaoUsuarios.get(i).getPontos() );
-		    
-			if (_pontuacaoUsuarios.get(i).getUsuario() == usuario &&
-				_pontuacaoUsuarios.get(i).getTipoPonto() == tipoPonto) {
-				
-				int p = _pontuacaoUsuarios.get(i).getPontos();
-				_totalPontos += p;
-			    System.out.println (" contador = " + _totalPontos );
+		    String name       = (String) _pontuacaoUsuarios.get(i).getUsuario();
+		    String typePoints = (String) _pontuacaoUsuarios.get(i).getTipoPonto();
+		    int points        = (int)    _pontuacaoUsuarios.get(i).getPontos();
+		    System.out.println(" REG=" + i + " -->)" + name + " , " + typePoints + " , " + points );
+			if (name.contains(usuario)) {
+				acheiUser = true;
+				if (typePoints.contains(tipoPonto)) {
+				    _totalPontos += points;
+			        System.out.println (" contador = " + _totalPontos );
+				}
 			}
+		}
+		if (!acheiUser) {
+			System.out.println ("nao achei o usuario " + usuario );
+			return 0;
 		}
 		return _totalPontos;
 	}
+	
+	private void exibe (String msg) {
+		System.out.println(msg);
+	}
+
 
 }
