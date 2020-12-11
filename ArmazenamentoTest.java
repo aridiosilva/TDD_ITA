ce6e9cf43af214968b1dc71f4f1646f0e10c78b4
Aridio Silva <aridiosilva@aridiosilva.com>
2020-12-11 10:48:05 -0300

    Add files via upload

--- ArmazenamentoTest.java
+++ ArmazenamentoTest.java
@@ -2,9 +2,8 @@
 package courseraita;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
+import java.util.LinkedList;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,13 +22,32 @@ class ArmazenamentoTest {
 	@Test
 	void testSet001() throws IOException, Exception {
 
-		DriverArquivoTXT _f =  new DriverArquivoTXT(); 
-		_f.deletarArquivoTextoDePontuacao();
-		Armazenamento _a = new Armazenamento(_f);
+		DriverArquivoTXT _driver =  new DriverArquivoTXT(false); 
+		Armazenamento _a = new Armazenamento(_driver);
+		
 		_a.armazenarPontuacaoDeUmUsuario(new PontuacaoUsuarios("joana", "estrela", 50));
 		_a.armazenarPontuacaoDeUmUsuario(new PontuacaoUsuarios("joana", "estrela", 50));
 		_a.armazenarPontuacaoDeUmUsuario(new PontuacaoUsuarios("joana", "comentarios", 80));
 
+		assertEquals (100, _a.recuperarTotaisDePontosDeUmTipoDeUmUsuario("estrela", "joana"));  
+		assertEquals (80, _a.recuperarTotaisDePontosDeUmTipoDeUmUsuario("comentarios", "joana"));  
+		
+		LinkedList<PontuacaoUsuarios> _listaPontos = _a.retornarUsuariosComAlgumTipodePonto();
+		
+		assertEquals ("joana",   _listaPontos.get(0).getUsuario()); 
+		assertEquals ("estrela", _listaPontos.get(0).getTipoPonto());  
+		assertEquals (50,        _listaPontos.get(0).getPontos());  
+
+		assertEquals ("joana",   _listaPontos.get(1).getUsuario()); 
+		assertEquals ("estrela", _listaPontos.get(1).getTipoPonto());  
+		assertEquals (50,        _listaPontos.get(1).getPontos());  
+		
+		assertEquals ("joana",   _listaPontos.get(2).getUsuario()); 
+		assertEquals ("comentarios", _listaPontos.get(2).getTipoPonto());  
+		assertEquals (80,        _listaPontos.get(2).getPontos());  
+		
+		DriverArquivoTXT _driver1 =  new DriverArquivoTXT(true); 
+		
 //    	_f.extendeArquivotextoPontuacao(new PontuacaoUsuarios("francisco", "vida", 19));
 //    	_f.extendeArquivotextoPontuacao(new PontuacaoUsuarios("francisco", "energia", 30));
 //    	_f.extendeArquivotextoPontuacao(new PontuacaoUsuarios("francisco", "curtida", 28));
@@ -37,7 +55,6 @@ class ArmazenamentoTest {
 //    	_f.extendeArquivotextoPontuacao(new PontuacaoUsuarios("francisco", "estrela", 100));
 //    	_f.extendeArquivotextoPontuacao(new PontuacaoUsuarios("francisco", "estrela", -50));
     	
-		assertEquals (280, _a.recuperarTotaisDePontosDeUmTipoDeUmUsuario("estrela", "joana"));  
 		
 	}
 	
